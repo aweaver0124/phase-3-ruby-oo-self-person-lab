1e93d759747406792e4f8a8a10701287a62ddca9
Alexandra Weaver <aweaver0124@gmail.com>
2022-01-13 11:23:38 -0600

    commit tree f5a733a124d39fa9ec6b243d82f95aefa3e37de5

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+  def happiness=num
+    if num > 10
+        @happiness = 10
+    elsif num < 0 
+        @happiness = 0
+    else
+        @happiness = num
+    end
+end
+
